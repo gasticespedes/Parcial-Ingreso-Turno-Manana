48dc6d3c26a699846936c223b3117e4731c410d1
Gaston Cespedes <gaston.cespedes1998@gmail.com>
2020-08-10 10:33:38 -0300

    Tercer punto

--- tres.js
+++ tres.js
@@ -11,5 +11,87 @@ c)el promedio de personas por viaje,  que viajan en invierno
 */
 function mostrar()
 {
-	alert("tres");
+	var respuesta;
+	var titular;
+	var lugar;
+	var temporada;
+	var cantidadPersonasQueViajan;
+	var lugarMasElegido;
+	var contadorBariloche = 0;
+	var contadorSalta = 0;
+	var contadorCataratas = 0;
+	var titularMaxPasajeros = 0;
+	var maxPasajeros = 0;
+	var titularPasajerosFlag = true;
+	var contadorViajes = 0;
+	var promedioPersonasPorViajeEnInvierno;
+	var contadorViajesInvierno = 0;
+	var acumuladorPersonasQueViajan = 0;
+	do{
+
+		do{
+			titular = prompt("Ingrese un titular");
+		}while (titular == false);
+
+		do{
+			lugar = prompt("Ingrese destino : Bariloche/Cataratas/Salta");
+			
+			if(lugar == "Bariloche "){
+					contadorBariloche++;
+				} else if (lugar == "Cataratas"){
+					contadorCataratas++;
+				}else {
+					contadorSalta++;
+				}
+		
+		}while (lugar != "Bariloche " || lugar != "Cataratas" 
+		|| lugar != "Salta");
+
+		do{
+			temporada = prompt("Ingrese temporada en la que deseea viajar "+
+			" Otoño/Invierno/Verano/Primavera");
+			if (temporada == "Invierno"){
+				contadorViajesInvierno++;
+			}
+
+		}while(temporada != "Otoño" || temporada != "Invierno" 
+		|| temporada != "Verano" || temporada != "Primavera");
+
+		do{
+			cantidadPersonasQueViajan = prompt("Ingrese la cantidad de viajeros");
+			cantidadPersonasQueViajan = parseInt(cantidadPersonasQueViajan);
+			acumuladorPersonasQueViajan = acumuladorPersonasQueViajan + cantidadPersonasQueViajan;
+
+		}while(cantidadPersonasQueViajan < 0 && isNaN(cantidadPersonasQueViajan));
+
+		contadorViajes++;
+
+	respuesta = prompt("Desea agregar otro destino ? Si/No");
+
+	}while(respuesta == "Si");
+
+
+	// PUNTO A
+	if(contadorBariloche > contadorCataratas && contadorBariloche > contadorSalta){
+		console.log("el lugar mas visitado es Bariloche");
+	}else if(contadorCataratas > contadorSalta){
+		console.log("el lugar mas visitado es Cataratas")
+	} else {
+		console.log ("el lugar mas visitado es Salta");
+	}
+//PUNTO B
+ if(titularPasajerosFlag == true ){
+	 titularPasajerosFlag == false 
+	 titularMaxPasajeros = titular;
+	 maxPasajeros = cantidadPersonasQueViajan;
+	 }else if (cantidadPersonasQueViajan > maxPasajeros){
+		 maxPasajeros = cantidadPersonasQueViajan;
+		 titularMaxPasajeros = titular
+	 }
+	 console.log("El titular que mas lleva es" + titular);
+
+	 // PUNTO C 
+promedioPersonasPorViajeEnInvierno = acumuladorPersonasQueViajan / contadorViajesInvierno;
+console.log("El promedio de las personas que viajan en invierno es" + promedioPersonasPorViajeEnInvierno);
+
 }
